8f4f9131ec14a6b2f308de7802e777b242473c8a
Alena <brand.18@gmail.com>
2020-01-25 10:48:30 -0500

    Updated project folder and main.js to be compatible with BootstrapVue and checked the functionality of the grid system.

--- website-test-mccain/src/App.vue
+++ website-test-mccain/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <Navbar />
+    <b-container class="bv-example-row">
+  <b-row>
+    <b-col>1 of 3</b-col>
+    <b-col>2 of 3</b-col>
+    <b-col>3 of 3</b-col>
+  </b-row>
+</b-container>
   </div>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-
 export default {
   name: "app",
   components: {
-    Navbar
   }
 };
 </script>
 
 <style lang="scss">
-@import './assets/styles/custom.scss';
-@import '../node_modules/bootstrap/scss/bootstrap.scss';
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
