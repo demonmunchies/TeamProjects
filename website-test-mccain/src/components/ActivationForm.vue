56729849ae048ecb4800a683a0b2e396ffb47a8d
Alena <brand.18@gmail.com>
2020-01-27 19:27:59 -0500

    Updated ActivationForm.vue to show characters in input.

--- website-test-mccain/src/components/ActivationForm.vue
+++ website-test-mccain/src/components/ActivationForm.vue
@@ -2,7 +2,7 @@
     <div>
   <b-form>
     <label for="text-activation-key">Activation Key</label>
-    <b-input type="password" id="text-activation-key"></b-input>
+    <b-input id="text-activation-key"></b-input>
    </b-form>
 </div>
 </template>
