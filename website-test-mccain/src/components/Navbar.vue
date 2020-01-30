431e2263ee820278c70920d0451ce6e06063e5b7
Alena <brand.18@gmail.com>
2020-01-29 22:03:41 -0500

    Uploading Navbar.vue to repository again.

--- website-test-mccain/src/components/Navbar.vue
+++ website-test-mccain/src/components/Navbar.vue
@@ -2,9 +2,7 @@
     <div>
   <b-navbar toggleable="lg" type="dark" variant="primary">
     <b-navbar-brand href="#">My Private Life</b-navbar-brand>
-
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/myprivatelife/">Home</b-nav-item>
