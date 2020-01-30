b46ce6cb3481dcf5b0f85026e8d74e255a13ee86
Alena <brand.18@gmail.com>
2020-01-29 22:06:34 -0500

    Started adding temperature scheduling components.

---
+++ website-test-mccain/src/components/TimeSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-form-input v-model="time" placeholder="Enter your name"></b-form-input>
+    <div class="mt-2">Value: {{ text }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        time: ''
+      }
+    }
+  }
+</script>
