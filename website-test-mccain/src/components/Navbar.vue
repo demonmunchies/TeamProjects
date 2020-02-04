4eb53406fbdc30a35f4cc8d54f70cc83fce1e1bd
Alena <brand.18@gmail.com>
2020-02-03 22:03:47 -0500

    Removed unnecessary components in Navbar.vue.

--- website-test-mccain/src/components/Navbar.vue
+++ website-test-mccain/src/components/Navbar.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
-  <b-navbar toggleable="lg" type="dark" variant="primary">
+  <b-navbar type="dark" variant="primary">
     <b-navbar-brand href="#">My Private Life</b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
+      <b-navbar-nav align="center">
         <b-nav-item to="/myprivatelife/">Home</b-nav-item>
         <b-nav-item to="/myprivatelife/thermostat-controls">Thermostat Controls</b-nav-item>
         <b-nav-item to="/myprivatelife/usage-statistics">Usage Statistics</b-nav-item>
         <b-nav-item to="/myprivatelife/user-manual">User Manual</b-nav-item>
       </b-navbar-nav>
-    </b-collapse>
   </b-navbar>
 </div>
 </template>
