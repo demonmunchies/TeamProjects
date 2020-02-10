f86527151e7e72f927429164aa40534ff9eb2556
Alena <brand.18@gmail.com>
2020-02-10 18:31:36 -0500

    Add date checking

--- website-test-mccain/src/components/DateSelect.vue
+++ website-test-mccain/src/components/DateSelect.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <label for="date-form">Select Date:</label>
-    <b-form-input v-model="date" type="date" v-on:input="updateDate"></b-form-input>
+    <b-form-input v-model="date" type="date" v-on:input="updateDate" :state="dateState" aria-describedby="invalid-feedback"></b-form-input>
+    <b-form-invalid-feedback :style="{'background-color': $store.state.backgroundColor, 'color': $store.state.textColor}">
+          <font size="2">Please select a date up to today</font>
+        </b-form-invalid-feedback>
   </div>
 </template>
 
@@ -13,8 +16,41 @@
       }
     },
     methods: {
+      checkDateState() {
+        var valid = false
+        var enteredDate = this.date.split('-')
+        var enteredYear = parseInt(enteredDate[0], 10)
+        var enteredMonth = parseInt(enteredDate[1], 10)
+        var enteredDay = parseInt(enteredDate[2], 10)
+
+        var currentDate = new Date()
+        var currentYear = currentDate.getFullYear()
+        var currentMonth = currentDate.getMonth() + 1
+        var currentDay = currentDate.getDate()
+        if(enteredYear <  currentYear)
+        {
+          valid = true
+        }
+        else if(enteredYear == currentYear && enteredMonth < currentMonth)
+        {
+          valid = true
+        }
+        else if(enteredYear == currentYear && enteredMonth == currentMonth && enteredDay <= currentDay)
+        {
+          valid = true
+        }
+        return valid
+      },
       updateDate() {
-        this.$store.dispatch("setDate", this.date)
+        if(this.checkDateState())
+        {
+          this.$store.dispatch("setDate", this.date)
+        }
+      }
+    },
+    computed: {
+      dateState() {
+        return this.checkDateState()
       }
     }
   }
