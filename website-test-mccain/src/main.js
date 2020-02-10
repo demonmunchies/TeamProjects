ea4afeb3e24af34ba8b38eb264fb04ca819e4573
Alena <brand.18@gmail.com>
2020-02-10 18:33:59 -0500

    Add keyboard shortcut plug in.

--- website-test-mccain/src/main.js
+++ website-test-mccain/src/main.js
@@ -7,6 +7,7 @@ import { BootstrapVue, IconsPlugin} from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin)
+Vue.use(require('vue-shortkey'))
 Vue.config.productionTip = false;
 
 new Vue({
