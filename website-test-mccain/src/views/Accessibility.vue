71b59fbbeac22dcc89ea94e4a553ef1db73e6121
Alena <brand.18@gmail.com>
2020-02-15 15:24:18 -0500

    Added functional keyboard shortcuts to Navbar.vue

--- website-test-mccain/src/views/Accessibility.vue
+++ website-test-mccain/src/views/Accessibility.vue
@@ -11,7 +11,12 @@
     <b-row class="mb-5"></b-row>
     <b-row>
       <b-col>Keyboard Shortcuts:</b-col>
-      <b-col></b-col>
+      <b-col>
+         <div> ctrl + l: Rotate webpage left</div>	
+        <div> ctrl + r: Rotate webpage right</div>	
+        <div> ctrl + up arrow: Increase Temperature</div>	
+        <div> ctrl + down arrow: Decrease Temperature</div>
+      </b-col>
       <b-col></b-col>
     </b-row>
   </div>
