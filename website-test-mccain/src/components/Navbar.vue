a0795c154a4fa348ae47bec9a64b5c6fb4400587
Alena <brand.18@gmail.com>
2020-02-15 17:00:13 -0500

    Update keyboard shortcuts.

--- website-test-mccain/src/components/Navbar.vue
+++ website-test-mccain/src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-  <b-navbar type="dark" variant="primary" v-shortkey="{left: ['ctrl', 'l'], right: ['ctrl', 'r']}" @shortkey="rotatePage">>
+  <b-navbar type="dark" variant="primary" 
+  v-shortkey="{left: ['ctrl', 'l'], right: ['ctrl', 'r'], accessibility: ['ctrl', 'a'], home: ['ctrl', 'm']}" @shortkey="rotatePage">>
     <b-navbar-brand href="#">My Private Life</b-navbar-brand>
       <b-navbar-nav align="center">
         <b-nav-item to="/myprivatelife/" exact exact-active-class="active">Home</b-nav-item>
@@ -16,29 +17,41 @@
 export default {
    methods: {
     rotatePage(event) {
-      var pageList= ["/myprivatelife/", "/myprivatelife/thermostat-controls", "/myprivatelife/usage-statistics", "/myprivatelife/user-manual"]
-      var currentPage = this.$router.currentRoute.path
-      for(var i = 0; i < pageList.length; i = i + 1){
-        if(pageList[i] == currentPage){
-          switch (event.srcKey) {
-            case 'right':
-              if(i == pageList.length - 1) {
-                this.$router.replace(pageList[0])
-              }
-              else {
-                this.$router.replace(pageList[i + 1])
-              }
-              break
-            case 'left':
-              if(i == 0) {
-                this.$router.replace(pageList[pageList.length - 1])
-              }
-              else {
-                this.$router.replace(pageList[i - 1])
-              }
-              break
+      switch (event.srcKey) {
+        case 'accessibility':
+          this.$router.replace("/myprivatelife/accessibility")
+          break
+        case 'home':
+          if(this.$router.currentRoute.path != "/myprivatelife/") {
+            this.$router.replace("/myprivatelife/")
           }
-        }
+          break
+        default:
+          var pageList= ["/myprivatelife/", "/myprivatelife/thermostat-controls", "/myprivatelife/usage-statistics", "/myprivatelife/user-manual"]
+          var currentPage = this.$router.currentRoute.path
+          for(var i = 0; i < pageList.length; i = i + 1){
+            if(pageList[i] == currentPage){
+              switch (event.srcKey) {
+                case 'right':
+                  if(i == pageList.length - 1) {
+                    this.$router.replace(pageList[0])
+                  }
+                  else {
+                    this.$router.replace(pageList[i + 1])
+                  }
+                  break
+                case 'left':
+                  if(i == 0) {
+                    this.$router.replace(pageList[pageList.length - 1])
+                  }
+                  else {
+                    this.$router.replace(pageList[i - 1])
+                  }
+                  break
+              }
+            }
+          }
+          break  
       }
     }
    }
--- website-test-mccain/src/views/Accessibility.vue
+++ website-test-mccain/src/views/Accessibility.vue
@@ -10,14 +10,16 @@
     </b-row>
     <b-row class="mb-5"></b-row>
     <b-row>
-      <b-col>Keyboard Shortcuts:</b-col>
-      <b-col>
-         <div> ctrl + l: Rotate webpage left</div>	
-        <div> ctrl + r: Rotate webpage right</div>	
+      <b-col class="text-right">Keyboard Shortcuts:</b-col>
+      <b-col class="text-left">
+        <div> ctrl + l: Rotate webpage left</div>	
+        <div> ctrl + r: Rotate webpage right</div>
+        <div> ctrl + a: Open Accessibility Settings</div>
+        <div> ctrl + m: Return to homepage</div>	
         <div> ctrl + up arrow: Increase Temperature</div>	
         <div> ctrl + down arrow: Decrease Temperature</div>
       </b-col>
-      <b-col></b-col>
+      <b-col cols="1"></b-col>
     </b-row>
   </div>
 </template>
