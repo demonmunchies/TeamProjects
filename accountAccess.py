0acdf1c7aa6a707ada9c79701874df217242c82a
RobF-WSU <59876172+RobF-WSU@users.noreply.github.com>
2020-03-26 12:58:34 -0400

    Added username/password change functionality

--- accountAccess.py
+++ accountAccess.py
@@ -16,6 +16,23 @@ def login(username, password):
             login_attempt = True
     return login_attempt
 
+def passChange(username,password,newpass):
+    change = False
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            user['password'] = newpass
+            change = True
+    return change
+
+def userChange(username,password,newuser):
+    change = False
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            user['username'] = newuser
+            change = True
+    return change
+
 if __name__ == '__main__':
     attempt = login(sys.argv[1],sys.argv[2])
     print(attempt)
+
