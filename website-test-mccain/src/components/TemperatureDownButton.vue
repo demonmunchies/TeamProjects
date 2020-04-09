f62300853149bf8a37ee281afb51f57dcf32cdda
Alena <brand.18@gmail.com>
2020-04-09 11:45:18 -0400

    Added server update desired temperature functionality.

--- website-test-mccain/src/components/TemperatureDownButton.vue
+++ website-test-mccain/src/components/TemperatureDownButton.vue
@@ -12,8 +12,28 @@ export default {
   },
   methods: {
       decrementDesiredTemperature() {
-        this.$store.commit("decrementDesiredTemperature");
+        if(this.$store.state.activated == true)
+        {
+          this.$store.commit("decrementDesiredTemperature");
+          var url = 'http://127.0.0.1:5000/update_desired_temperature';
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", url, true);
+          xhr.setRequestHeader("Content-Type", "application/json")
+          xhr.onreadystatechange = function() {
+              if(xhr.readyState == 4) {
+                xhr.response;
+              }
+          }
+          xhr.onerror = function(err)
+          {
+              console.warn(err);
+          }
+          let json = JSON.stringify({
+  data: this.$store.state.desiredTemperature
+  })
+          xhr.send(json)
+        }
       }
     }
-}
+  }
 </script>
--- website-test-mccain/src/components/TemperatureUpButton.vue
+++ website-test-mccain/src/components/TemperatureUpButton.vue
@@ -12,7 +12,28 @@ export default {
   },
   methods: {
       incrementDesiredTemperature() {
-        this.$store.commit("incrementDesiredTemperature");
+        if(this.$store.state.activated == true)
+        {
+          this.$store.commit("incrementDesiredTemperature");
+          var url = 'http://127.0.0.1:5000/update_desired_temperature';
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", url, true);
+          xhr.setRequestHeader("Content-Type", "application/json")
+          xhr.onreadystatechange = function() {
+              if(xhr.readyState == 4) {
+                xhr.response;
+              }
+          }
+          xhr.onerror = function(err)
+          {
+              console.warn(err);
+          }
+
+          let json = JSON.stringify({
+          data: this.$store.state.desiredTemperature
+          })
+          xhr.send(json)
+        }
       }
     }
 }
