03cd4e3ab7876d2369c74a5b03484d2eb7bcc9ed
Alena <brand.18@gmail.com>
2020-04-09 11:48:24 -0400

    Implement dynamic graph updating.

--- website-test-mccain/src/components/DateSelect.vue
+++ website-test-mccain/src/components/DateSelect.vue
@@ -3,7 +3,7 @@
     <label for="date-form">Select Date:</label>
     <b-form-input v-model="date" type="date" v-on:input="updateDate" :state="dateState" aria-describedby="invalid-feedback"></b-form-input>
     <b-form-invalid-feedback :style="{'background-color': $store.state.backgroundColor, 'color': $store.state.textColor}">
-          <font size="2">Please select a date up to today</font>
+          <font size="2">Please select a date up to today and make sure you are logged in </font>
         </b-form-invalid-feedback>
   </div>
 </template>
@@ -44,13 +44,49 @@
       updateDate() {
         if(this.checkDateState())
         {
-          this.$store.dispatch("setDate", this.date)
+          if(this.$store.state.activated == true)
+          {
+            var enteredDate = this.date.split('-')
+            this.$store.dispatch("setDay", parseInt(enteredDate[2], 10))
+            this.$store.dispatch("setMonth", parseInt(enteredDate[1], 10))
+            this.$store.dispatch("setYear", parseInt(enteredDate[0], 10))
+            this.updateGraph()
+          }
         }
+      },
+      updateGraph() {
+        var url = 'http://127.0.0.1:5000/get_singular_graph';
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4) {
+              console.log("");
+            }
+        }
+        xhr.onerror = function(err)
+        {
+            console.warn(err);
+        }
+           let json = JSON.stringify({
+          select : this.$store.state.range, 
+          day : this.$store.state.day, 
+          month : this.$store.state.month, 
+          year : this.$store.state.year
+        });
+        xhr.send(json)
+        if(xhr.status == 200)
+        {
+          this.forceRerender()
+        }
+      },
+      forceRerender() {
+        this.$store.commit("incrementGraphComponentKey");
       }
     },
     computed: {
       dateState() {
-        return this.checkDateState()
+        return this.$store.state.activated && this.checkDateState()
       }
     }
   }
--- website-test-mccain/src/components/RangeForm.vue
+++ website-test-mccain/src/components/RangeForm.vue
@@ -12,16 +12,47 @@
         selected: null,
         options: [
           { value: null, text: 'Please select a time frame' },
-          { value: 'day', text: 'Day' },
-          { value: 'week', text: 'Week' },
-          { value: 'month', text: 'Month' },
-          { value: 'year', text: 'Year' }
+          { value: 1, text: 'Day' },
+          { value: 2, text: 'Week' },
+          { value: 3, text: 'Month' },
+          { value: 4, text: 'Year' }
         ]
       }
      },
      methods: {
       updateRange() {
-        this.$store.dispatch("setRange", this.selected)
+        if(this.$store.state.activated == true)
+        {
+          this.$store.dispatch("setRange", this.selected)
+          this.updateGraph()
+        }
+      },
+      updateGraph() {
+        var url = 'http://127.0.0.1:5000/get_singular_graph';
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.onreadystatechange = function() {
+        }
+        xhr.onerror = function(err)
+        {
+            console.warn(err);
+        }
+        let json = JSON.stringify({
+        select : this.$store.state.range, 
+        day : this.$store.state.day, 
+        month : this.$store.state.month, 
+        year : this.$store.state.year
+        });
+        xhr.send(json)
+        if(xhr.status == 200)
+        {
+          this.forceRerender()
+        }
+      },
+      forceRerender() {
+        this.$store.commit("incrementGraphComponentKey");
       }
     }
   }
