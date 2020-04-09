df90d9b0f109e76bb42adb246168ceb005110081
Alena <brand.18@gmail.com>
2020-04-09 11:52:11 -0400

    Update to check login credentials.

--- website-test-mccain/src/components/LoginPane.vue
+++ website-test-mccain/src/components/LoginPane.vue
@@ -19,6 +19,7 @@
       </div>
     </b-row>
     <b-row><b-button v-on:click="loginCheck" v-shortkey="['ctrl', 'enter']" @shortkey="loginCheck">Submit</b-button></b-row>
+    <p>{{ clickText }}</p>
 </b-container>
 </template>
 
@@ -29,12 +30,68 @@ export default {
   data() {
     return {
       activationKey: '',
-      password: ''
+      password: '',
+      clickText: ''
     }
   },
   methods: {
     loginCheck(){
-      console.log("submit clicked")
+        var comp = this;
+        var url = 'http://127.0.0.1:5000/login';
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4) {
+              var sucess = JSON.parse(this.responseText);
+              if(sucess == true)
+              {
+                comp.$store.dispatch("setCurrentActivationKey", comp.activationKey)
+                comp.$store.dispatch("setActivated", true)
+                comp.clickText = 'You are logged in'
+                comp.activationKey = ''
+                comp.password = ''
+                comp.initializeSchedule()
+
+              }
+              else {
+                comp.$store.dispatch("setCurrentActivationKey", "")
+                comp.$store.dispatch("initializeSchedule", [])
+                comp.$store.dispatch("setActivated", false)
+                comp.clickText = 'Error: Incorrect activation key or password'
+                comp.activationKey = ''
+                comp.password = ''
+              }
+            }
+        }
+        xhr.onerror = function(err)
+        {
+            console.warn(err);
+        }
+
+          let json = JSON.stringify({
+          user: this.activationKey,
+          passwd: this.password
+        });
+        xhr.send(json)
+    },
+    initializeSchedule(){
+      var comp = this
+      var url = 'http://127.0.0.1:5000/get_schedule';
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.setRequestHeader("Content-Type", "application/json")
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            comp.$store.dispatch("initializeSchedule", JSON.parse(xhr.response))
+        }
+      } 
+
+      xhr.onerror = function(err)
+      {
+          console.warn(err);
+      }
+      xhr.send();
     }
   }
 };
